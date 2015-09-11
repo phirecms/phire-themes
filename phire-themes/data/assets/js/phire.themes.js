8cb9e537c12eec5ff72e3ba981a92be279186e81
Nick Sagona <nick@nolainteractive.com>
2015-09-11 16:13:48 -0500

    Update JS

--- phire-themes/data/assets/js/phire.themes.js
+++ phire-themes/data/assets/js/phire.themes.js
@@ -1,3 +1,15 @@
 /**
  * Themes Module Scripts for Phire CMS 2
  */
+
+jax(document).ready(function(){
+    if (jax('#themes-form')[0] != undefined) {
+        jax('#checkall').click(function(){
+            if (this.checked) {
+                jax('#themes-form').checkAll(this.value);
+            } else {
+                jax('#themes-form').uncheckAll(this.value);
+            }
+        });
+    }
+});
